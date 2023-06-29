89af108cc48f94be5b357e28c0ba8da27bf19758
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2023-06-29 11:29:25 -0300

    Update pid.cpp

--- pid.cpp
+++ pid.cpp
@@ -11,14 +11,33 @@ float i = 0;
 float d = 0;
 float prev_e = 0;
 
+float feedforwardDisturb = 0; // Valor esperado do distúrbio
+
 void setup()
 {
   Serial.begin(9600);
+  randomSeed(analogRead(0)); // Inicializa a semente para gerar números aleatórios
 }
 
 void loop()
 {
+  if (Serial.available() > 0) {
+    sp = Serial.parseFloat(); // Lê o valor digitado no Serial Monitor e atualiza o setpoint (sp)
+  	Serial.print("Setpoint alterado para: ");
+    Serial.println(sp);
+  }
+  
   y = map(analogRead(A0), 20, 358, -40, 125); // leitura da variável do processo
+  
+  // Verifica se deve ocorrer o distúrbio de forma aleatória
+  if (random(3) == 0) {
+    float disturbio = random(-3, 3); // Gera um valor aleatório entre -3 e 3 para o distúrbio
+    y += disturbio; // Adiciona o distúrbio à temperatura
+    feedforwardDisturb = disturbio; // Define o valor esperado do distúrbio
+  } else {
+    feedforwardDisturb = 0; // Sem distúrbio
+  }
+  
   Serial.print("VP: ");
   Serial.println(y);
   
@@ -28,10 +47,14 @@ void loop()
 
   p = kp * e; // controle proporcional
   i += ki * h * e; // controle integral
+  
+  // Controle feedforward
+  float ff = kp * feedforwardDisturb; // Termo de feedforward
+  
   d = kd * (e - prev_e) / h; // controle derivativo
   prev_e = e;
 
-  float u = p + i + d; // sinal de controle (variável manipulada)
+  float u = p + i + d + ff; // sinal de controle (variável manipulada)
   u = map(u, -4098, 4098, 0, 255);
   Serial.print("MV = ");
   Serial.println(u);
